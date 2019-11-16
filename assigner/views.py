6347fa5f0d8c8e17e8253f0c0d7253b0085eec24
Nadeen Gamage <nadeengamage@gmail.com>
2019-11-16 20:19:22 +0530

    Assigner index view created

--- assigner/views.py
+++ assigner/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import render
+from .models import Assigner
 
-# Create your views here.
+
+def index(request):
+	assigner_list = Assigner.objects.all()
+	paginator = Paginator(assigner_list, 10)
+
+	page = request.GET.get('page')
+	assigners = paginator.get_page(page)
+
+	return render(request, 'assigner/views/index.html', {'assigners': assigners})
