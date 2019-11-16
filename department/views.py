c748e5dd0307ce6e225fb9d908be30232f0262eb
Nadeen Gamage <nadeengamage@gmail.com>
2019-11-16 20:20:06 +0530

    Department index view created

--- department/views.py
+++ department/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import render
+from .models import Department
 
-# Create your views here.
+def index(request):
+	department_list = Department.objects.all()
+	paginator = Paginator(department_list, 10)
+
+	page = request.GET.get('page')
+	departments = paginator.get_page(page)
+
+	return render(request, 'department/views/index.html', {'departments': departments})
