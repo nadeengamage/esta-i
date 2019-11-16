ec3373f50ab0194f6fe418ef2cac0f2f5e099ee7
Nadeen Gamage <nadeengamage@gmail.com>
2019-11-16 20:19:46 +0530

    Registerd all domain URLs

--- assignment_1/urls.py
+++ assignment_1/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+	path('', include('home.urls')),
     path('admin/', admin.site.urls),
+    path('employee/', include('employee.urls')),
+    path('assigner/', include('assigner.urls')),
+    path('department/', include('department.urls'))
 ]
