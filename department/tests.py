718ea74ff23f488bfbadbf87ad34f601d179244d
Nadeen Gamage <nadeengamage@gmail.com>
2019-11-16 20:56:14 +0530

    Department test case created

--- department/tests.py
+++ department/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import Department
 
-# Create your tests here.
+class DepartmentTestCase(TestCase):
+	
+	def setUp(self):
+		Department.objects.create(name='Department1', working_days_per_week='5', working_hours_per_day='08:00:00', status=1)
+		Department.objects.create(name='Department2', working_days_per_week='5', working_hours_per_day='09:00:00', status=1)
+		Department.objects.create(name='Department3', working_days_per_week='5', working_hours_per_day='09:00:00', status=1)
+		Department.objects.create(name='Department4', working_days_per_week='5', working_hours_per_day='08:00:00', status=1)
+		Department.objects.create(name='Department5', working_days_per_week='5', working_hours_per_day='09:00:00', status=1)
+
+	def test_department_created(self):
+		department1 = Department.objects.get(name='Department1')
+		department2 = Department.objects.get(name='Department3')
+		department3 = Department.objects.get(name='Department5')
+
+		self.assertEqual(str(department1.working_hours_per_day), '08:00:00')
+		self.assertEqual(str(department2.working_hours_per_day), '09:00:00')
+		self.assertEqual(str(department3.working_hours_per_day), '09:00:00')
+		
